1ef87f7cd04a1dc7379c7212a54d1a4f839bebc2
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2022-11-17 23:40:51 +0000

    animation auto on resizing

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ class Symbol {
     }
     draw(context){
         this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length))
-        context.fillStyle = '#0aff0a'
+        
         context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if (this.y * this.fontSize > this.canvasHeight){
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > .98){
             this.y = 0;
         }else {
             this.y += 1;
@@ -43,15 +43,34 @@ class Effect {
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 60;
+const nextFrame = 1000/fps;
+let timer = 0;
 
-function animate() {
-    // draw fade away over canvas
-    ctx.fillStyle = 'rgba(0, 0, 0, .05)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // end
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
+function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    if (timer > nextFrame){
+        // draw fade away over canvas
+        ctx.fillStyle = 'rgba(0, 0, 0, .05)';
+        ctx.textAlign = 'center';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = '#0aff0a'
+        // end
+        ctx.font = effect.fontSize + 'px monospace';
+        effect.symbols.forEach(symbol => symbol.draw(ctx));
+        timer = 0;
+    }else{
+        timer += deltaTime;
+    }
+
     requestAnimationFrame(animate);
 }
 
-animate();
+animate(0);
+
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
